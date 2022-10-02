fb5f20bd25f617f79e0b80222c8a31b48b66729d
braedon911 <53923231+braedon911@users.noreply.github.com>
2022-10-02 19:03:49 -0400

    created actor and solid scripts

---
+++ Assets/Scripts/Solid.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Solid : Actorsolid
+{
+    private bool collidable = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Move(float x, float y)
+    {
+
+        
+    }
+}
