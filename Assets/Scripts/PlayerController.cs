17c5f1a0fbb04a714036025a4bf53b2be5d0daa5
Cosmocs <101135810+Cosmocs@users.noreply.github.com>
2022-10-03 19:31:12 -0400

    added player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Actor actor;
+    public CollisionBox box = actor.box;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
